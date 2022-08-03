7ab4b37b98a9d57a3e02e545a0f0ce8312ace22f
Majoxs <majoxs@gmail.com>
2022-08-03 14:18:41 +0500

    Markup of a simple landing page

---
+++ src/js/_vars.js
@@ -0,0 +1,13 @@
+export default {
+  windowEl: window,
+  documentEl: document,
+  htmlEl: document.documentElement,
+  bodyEl: document.body,
+  menuArrows: document.querySelectorAll('.menu__arrow'),
+  menuLinks: document.querySelectorAll('.menu__link[data-goto]'),
+  iconMenu: document.querySelector('.menu__icon'),
+  menuBody: document.querySelector('.menu__body'),
+  navContainer: document.querySelector('.nav__container'),
+  bannerSlider: document.querySelector('.banner-slider'),
+  rentSlider: document.querySelector('.rent-slider'),
+}
